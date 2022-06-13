06bc737333d349136a70511e4a61cc23f5c14aee
herr <wanghe_hi@qq.com>
2022-06-13 18:06:09 +0800

    test jump

---
+++ src/components/WelcomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="welcomePage">
+    <div>useless page
+      <ul>
+        <li>
+          <a href="../../useless-page/kill-myself/index.html">useless</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WelcomePage"
+}
+</script>
+
+<style scoped>
+
+</style>
